65de30f78ae69c402219d64b81aeac8f4009189f
2667436740 <2667436740@qq.com>
2022-04-09 00:21:43 +0800

    '修改配置'

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 // var db = mongoose.createConnection('mongodb://localhost:27017/closechat')
 //连接   某数据库且带用户密码  的mongodb
-var db = mongoose.createConnection('mongodb://[name]:[pwd]@[ip]:[port]/[dbName]?authSource=admin')
+var db = mongoose.createConnection('mongodb://root:ZXCzxc1234560@124.222.244.60:12121/closechat?authSource=admin')
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => {
   console.info('数据库closechat打开成功')
