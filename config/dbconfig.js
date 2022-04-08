fbd22b82a774233f99a5cdc2f97affeb294e52a3
2667436740 <2667436740@qq.com>
2022-04-09 00:33:47 +0800

    '重置git上传文件'

---
+++ config/dbconfig.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+// var db = mongoose.createConnection('mongodb://localhost:27017/closechat')
+//连接   某数据库且带用户密码  的mongodb
+var db = mongoose.createConnection('mongodb://[user]:[pwd]@[ip]:[port]/[db]?authSource=admin')
+db.on('error', console.error.bind(console, 'connection error'))
+db.once('open', () => {
+  console.info('数据库closechat打开成功')
+})
+
+module.exports = db
